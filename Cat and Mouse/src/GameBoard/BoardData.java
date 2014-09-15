d1051cc5eb141aa1cf12ca2da9b6ae2c5d9e5a22
DanHenton <Dan.Henton@live.com>
2014-09-15 23:22:19 +1200

    Removed my test main()

--- Cat and Mouse/src/GameBoard/BoardData.java
+++ Cat and Mouse/src/GameBoard/BoardData.java
@@ -9,12 +9,9 @@ public class BoardData {
 	private int tileCount = 20; 
 	private byte[][] groundLayer = new byte[tileCount][tileCount];
 	private byte[][] objectLayer = new byte[tileCount*2][tileCount*2];	
+	
 	public BoardData(){
 		
-	}
-	
-	public static void main(String[] args){
-		new BoardData();
 	}
 }
 
