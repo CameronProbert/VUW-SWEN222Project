6fdac38945920bd4f26b14c724b73b8c591dd099
DanHenton <Dan.Henton@live.com>
2014-09-16 00:38:04 +1200

    Added TestBoard.scv File and FileReader for the rooms

--- Cat and Mouse/src/GameBoard/BoardData.java
+++ Cat and Mouse/src/GameBoard/BoardData.java
@@ -1,18 +1,36 @@
 package GameBoard;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 /**
  * 
  * @author Dan
- *
- *stores all of the boards data
+ * 
+ *         stores all of the boards data
  */
 public class BoardData {
-	private int tileCount = 20; 
-	private byte[][] groundLayer = new byte[tileCount][tileCount];
-	private byte[][] objectLayer = new byte[tileCount*2][tileCount*2];	
-	
-	public BoardData(){
-		
+
+	private Room[] levelOne;
+
+	public BoardData() {
+
 	}
+
+	/**
+	 * TODO
+	 * 
+	 * @param levelSize
+	 *            the amount of rooms in the level
+	 * @return
+	 */
+	public Room[] loadLevel(int levelSize) {
+		return new Room[levelSize];
+	}
+
+	public static void main(String[] args) {
+		// Test File reading and converting to bytes
+		new Room("TestBoard.csv", "TestBoard.csv");
+	}
+
 }
-
-
