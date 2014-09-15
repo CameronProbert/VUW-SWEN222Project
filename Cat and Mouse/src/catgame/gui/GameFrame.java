8c85fa4dece12f290eafc6649ed8714772bc1e5f
CameronProbert <c.probert@hotmail.co.nz>
2014-09-15 20:25:34 +1200

    Created the GameFrame
    This will display the game client

---
+++ Cat and Mouse/src/catgame/gui/GameFrame.java
@@ -0,0 +1,15 @@
+package catgame.gui;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
