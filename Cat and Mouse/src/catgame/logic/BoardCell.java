dcf3b3f6be08d6fe691d996c0eb699f46f5e8739
Ludmila Dymnikova <mila.dymnikova@gmail.com>
2014-09-24 14:27:54 +1200

    Draw methods in BoardCell added.

--- Cat and Mouse/src/catgame/logic/BoardCell.java
+++ Cat and Mouse/src/catgame/logic/BoardCell.java
@@ -1,22 +1,45 @@
 package catgame.logic;
 
 import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import catgame.GameObjects.GameObject;
 
 public class BoardCell {
 	Position cellPosition;
 	GameObject objectOnCell;
-	String groundFile;
+	Image groundImage;
+	Image objectImage;
 	
 	public BoardCell(Position position, GameObject object , String groundFile){
 		this.cellPosition = position;
 		this.objectOnCell = object;
-		this.groundFile = groundFile;
+		
+		try {
+			groundImage = ImageIO.read(new File(groundFile));
+			objectImage = ImageIO.read(new File(objectOnCell.getObjectID()+""));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void drawCell(Graphics g){
-		g.drawImage(groundFile, cellPosition.getX() , cellPosition.getY())
+	/**
+	 * draw the cells and there objects
+	 * @param g
+	 */ 
+	public void drawCellBase(Graphics g){
+		g.drawImage(groundImage, cellPosition.getX() , cellPosition.getY(), null);
 	}
 	
+	/**
+	 * Draw the object on the cell
+	 * @param g
+	 */
+	public void drawCellObject(Graphics g){
+		g.drawImage(objectImage, cellPosition.getX() , cellPosition.getY(), null);
+	}
 }
