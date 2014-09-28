27589e91b252c92bb37bb95485cfb3832f655219
Francinebc <francinebc.mail@gmail.com>
2014-09-28 20:52:35 +1300

    Fixed Updates in ClientServer
    Added in correct logic for opening box and attacking
    Need to sort out what happens when moved to new room
    Also need to sort out clicking on inventory items

--- Cat and Mouse/src/catgame/gui/ClientFrame.java
+++ Cat and Mouse/src/catgame/gui/ClientFrame.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import catgame.GameObjects.Chest;
 import catgame.clientserver.NetworkHandler;
 import catgame.clientserver.Update;
 import catgame.gui.renderpanel.RenderPanel;
@@ -21,12 +22,14 @@ public class ClientFrame extends AbstractFrame implements KeyListener {
 	private NetworkHandler network;
 	private int clientsUID;
 	private RenderPanel renderPanel;
+	private boolean isClient;
 
-	public ClientFrame(NetworkHandler network, int UID) {
+	public ClientFrame(NetworkHandler network, int UID, boolean isClient) {
 		super(new Dimension(1200, 600), "Cat and Mouse");
 		this.addPanels();
 		this.clientsUID = UID;
 		this.network = network;
+		this.isClient = isClient;
 	}
 
 	private void addPanels() {
@@ -37,11 +40,14 @@ public class ClientFrame extends AbstractFrame implements KeyListener {
 		this.add(invPanel);
 		this.add(statPanel);
 	}
-	
+
+	/**
+	 * NOTE: may need to check when moving if moved to another room!! TODO
+	 */
 	@Override
 	public void keyPressed(KeyEvent key) {
 		int keyID = key.getID();
-		boolean validAction = false;
+		int validAction = 0;
 		Update up = new Update(0);
 		switch (keyID) {
 		case KeyEvent.VK_W:
@@ -61,29 +67,42 @@ public class ClientFrame extends AbstractFrame implements KeyListener {
 			up = new Update(Update.Descriptor.EAST, clientsUID, 0);
 			break;
 		case KeyEvent.VK_SPACE:
-			int attackTarget = network.getGameMain().attack(clientsUID);
-			up = new Update(Update.Descriptor.ATTACK, clientsUID, attackTarget);
+			int attacked = network.getGameMain().attack(clientsUID);
+			if(attacked>0){
+				validAction = 1;
+				up = new Update(Update.Descriptor.ATTACK, clientsUID, attacked);
+			}
+			break;
+		case KeyEvent.VK_E: //  open a chest
+			Chest chest = network.getGameMain().getChest(clientsUID);
+			if(chest!=null){
+				validAction = 1;
+				// TODO open dialog box to choose items out of chest (using the chest object)
+				// int objectID = openChest(chest); // return the id of the item the character selected
+				// up = new Update(Update.Descriptor.PICKUP, clientsUID, objectID);
+			}
 			break;
 		}
-		if (validAction) {
+		if (validAction > 0 && isClient) {
 			network.update(up, true);
 		}
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public static void main(String[] args){
-		new ClientFrame(null, 0);
+		new ClientFrame(null, 0, true);
 	}
 
 }
--- Cat and Mouse/src/catgame/logic/GameMain.java
+++ Cat and Mouse/src/catgame/logic/GameMain.java
@@ -3,6 +3,7 @@ package catgame.logic;
 import java.util.HashMap;
 
 import catgame.GameObjects.MasterObject;
+import catgame.GameObjects.Chest;
 
 public class GameMain {
 
@@ -18,27 +19,59 @@ public class GameMain {
 		return toLoad;
 	}
 
-	public boolean moveNorth(int playerID) {
-		return false; // TODO gameMap.get(playerID).move("NORTH");
+	/**
+	 * 
+	 * @param playerID
+	 * @return whether a successful move or not (-1 = not success, 1 = success)
+	 */
+	public int moveNorth(int playerID) {
+		return -1; // TODO gameMap.get(playerID).move("NORTH");
 	}
 
-	public boolean moveSouth(int playerID) {
-		return false; // TODO gameMap.get(playerID).move("SOUTH");
+	/**
+	 * 
+	 * @param playerID
+	 * @return whether a successful move or not (-1 = not success, 1 = success)
+	 */
+	public int moveSouth(int playerID) {
+		return -1; // TODO gameMap.get(playerID).move("SOUTH");
 	}
 
-	public boolean moveEast(int playerID) {
-		return false; // TODO gameMap.get(playerID).move("EAST");
+	/**
+	 * 
+	 * @param playerID
+	 * @return whether a successful move or not (-1 = not success, 1 = success)
+	 */
+	public int moveEast(int playerID) {
+		return -1; // TODO gameMap.get(playerID).move("EAST");
 	}
 
-	public boolean moveWest(int playerID) {
-		return false; // TODO gameMap.get(playerID).move("WEST");
+	/**
+	 * 
+	 * @param playerID
+	 * @return whether a successful move or not (-1 = not success, 1 = success)
+	 */
+	public int moveWest(int playerID) {
+		return -1; // TODO gameMap.get(playerID).move("WEST");
 	}
 
+	/**
+	 * 
+	 * @param playerID
+	 * @return whether could attack or not, -1 for not an attack 
+	 * and if it could attack return the id of what was attacked
+	 */
 	public int attack(int playerID) {
 		// TODO find the target and attack it, then return the ID of the target
 		return -1; // should return the attackerID
 	}
 
+	/**
+	 * 
+	 * @param playerID
+	 * @return whether could attack or not, -1 for not an attack 
+	 * and if it could attack return the id of what was attacked
+	 */
 	public int attack(int playerID, int attackedID) {
 		//TODO attack the target
 		return attackedID; // should return the attackerID (though it is
@@ -61,5 +94,15 @@ public class GameMain {
 
 	public boolean moveToNextRoom(int playerID, int roomID) {
 		return false;
+	}
+
+	/**
+	 * Return the chest object that the player is standing infront of, return null if none
+	 * @param clientsUID
+	 * @return
+	 */
+	public Chest getChest(int clientsUID) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
