eddd699bb270c356b9206603e2d90cf521705de7
MichaelGordon <fallthroughinfinity@gmail.com>
2014-10-01 19:38:38 +1300

    RenderPanel now uses two methods for drawing objects. getObjectsFromArray() is first called, this finds any objects marked in the testObjects 2D Array, it then determines the type of object at each position and calls drawObjectsFromArray(), passing the relevant image file and position values for the drawObjectsFromArray to draw the images.

--- Cat and Mouse/src/catgame/gui/renderpanel/RenderPanel.java
+++ Cat and Mouse/src/catgame/gui/renderpanel/RenderPanel.java
@@ -20,12 +20,15 @@ public class RenderPanel extends JPanel {
 	public final ClientFrame parentFrame;
 	private Image grassBlock;
 	private Image tree1;
+	private Image tree2;
+	private Image bush1;
+	private Image rock1;
 	public viewDirection viewDir = viewDirection.NORTH;
 	
 	//Test 2D array for grassBlocks
 	private int[][] testMap = {
 			{1, 1, 1, 1, 1, 1, 1, 1}, 
-			{1, 0, 0, 1, 1, 1, 1, 1}, 
+			{1, 1, 1, 1, 1, 1, 1, 1}, 
 			{1, 1, 1, 1, 1, 1, 1, 1},
 			{1, 1, 1, 1, 0, 0, 0, 1},
 			{1, 1, 1, 1, 0, 0, 0, 1},
@@ -34,13 +37,13 @@ public class RenderPanel extends JPanel {
 	};
 	
 	private int[][] testObjects = {
-			{1, 0, 0, 0, 0, 0, 0, 0},
-			{0, 1, 0, 0, 0, 0, 0, 0},
+			{1, 3, 1, 0, 0, 0, 0, 0},
+			{3, 3, 3, 0, 0, 0, 2, 0},
+			{1, 3, 0, 0, 0, 0, 0, 0},
 			{0, 0, 0, 0, 0, 0, 0, 0},
-			{0, 0, 0, 1, 1, 0, 0, 0},
 			{0, 0, 0, 0, 0, 0, 0, 0},
-			{0, 0, 0, 0, 0, 1, 0, 0},
-			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 2, 0},
+			{0, 0, 4, 4, 3, 0, 3, 0},
 	};
 	
 	public enum viewDirection{
@@ -71,53 +74,32 @@ public class RenderPanel extends JPanel {
 		try {
 			grassBlock = ImageIO.read(RenderPanel.class.getResource("/images/Grass1.png"));
 			tree1 = ImageIO.read(RenderPanel.class.getResource("/images/Tree1.png"));
+			tree2 = ImageIO.read(RenderPanel.class.getResource("/images/Tree2.png"));
+			bush1 = ImageIO.read(RenderPanel.class.getResource("/images/Bush1.png"));
+			rock1 = ImageIO.read(RenderPanel.class.getResource("/images/Rock1.png"));
 		} catch (IOException e) {
 			System.out.println("There was an issue loading image files, check file locations are correct.");
 			e.printStackTrace();
 		}
+	}	
+
+	public viewDirection getViewDir(){
+		return viewDir;
+	}
+
+	public void setViewDir(viewDirection vd){
+		viewDir = vd;
 	}
 	
-//	public void drawGrass(Graphics g){
-//		g.setColor(Color.DARK_GRAY);
-//		g.fillRect(0, 0, 1200, 600);
-//		int startX1 = 19 + 500;
-//		int startY1 = 20 + 200;
-//		int startX2 = 81 + 500;
-//		int startY2 = 55 + 200;
-//		int incrX = 124;
-//		int incrY = 70;
-//		g.drawImage(grassBlock, startX1, startY1, null);
-//		g.drawImage(grassBlock, startX1+incrX, startY1, null);
-//		g.drawImage(grassBlock, startX1+incrX*2, startY1, null);
-//		g.drawImage(grassBlock, startX1+incrX*3, startY1, null);
-//		g.drawImage(grassBlock, startX1+incrX*4, startY1, null);
-//		
-//		g.drawImage(grassBlock, startX2, startY2, null);
-//		g.drawImage(grassBlock, startX2+incrX, startY2, null);
-//		g.drawImage(grassBlock, startX2+incrX*2, startY2, null);
-//		g.drawImage(grassBlock, startX2+incrX*3, startY2, null);
-//		
-//		g.drawImage(grassBlock, startX1+incrX, startY1+incrY, null);
-//		g.drawImage(grassBlock, startX1+incrX*2, startY1+incrY, null);
-//		g.drawImage(grassBlock, startX1+incrX*3, startY1+incrY, null);
-//		
-//		g.drawImage(grassBlock, startX2+incrX, startY2+incrY, null);
-//		g.drawImage(grassBlock, startX2+incrX*2, startY2+incrY, null);
-//		
-//		g.drawImage(grassBlock, startX1+incrX*2, startY1+incrY*2, null);
-//	}
 	
 	//testing method for drawing grass blocks form 2D array
 	public void testDrawGrass(Graphics g){
-		int startX1 = 19 + parentFrame.getWidth() / 2;
+		int startX1 = 19 + parentFrame.getWidth() / 4;
 		int startY1 = 20 + 400;
-		int startX2 = 81 + parentFrame.getWidth() / 2;
-		int startY2 = 55 + 400;
 		int incrX = 124;
 		int incrY = 70;
-		int xCount = 10;
-		int yCount = 10;
-		g.fillRect(parentFrame.getWidth() / 2, 200, 20, 20);
+//		int xCount = 10;
+//		int yCount = 10;
 		for (int i = 0; i < testMap.length; i++){
 			for (int j = testMap[0].length; j > 0; j--){
 				//System.out.println("i: " + i + " j: " + j);
@@ -127,46 +109,74 @@ public class RenderPanel extends JPanel {
 							startY1 + (i * incrY / 2) - (j * incrY / 2), 
 							null);
 				}
-				g.setColor(Color.BLUE);
-				if (i == 0){
-					g.fillRect(
-							startX1 + (j * incrX / 2) + (i * incrX / 2), 
-							startY1 + (i * incrY / 2) - (j * incrY / 2), 
-							xCount, 
-							xCount);
-					xCount = xCount + 2;
-				}
-				g.setColor(Color.RED);
-				if (j == testMap[0].length){
-					g.fillRect(
-							startX1 + ((j+1) * incrX / 2) + ((i+1) * incrX / 2), 
-							startY1 + (i * incrY / 2) - (j * incrY / 2), 
-							yCount, 
-							yCount);
-					yCount = yCount + 2;
-				}
+//				g.setColor(Color.BLUE);
+//				if (i == 0){
+//					g.fillRect(
+//							startX1 + (j * incrX / 2) + (i * incrX / 2), 
+//							startY1 + (i * incrY / 2) - (j * incrY / 2), 
+//							xCount, 
+//							xCount);
+//					xCount = xCount + 2;
+//				}
+//				g.setColor(Color.RED);
+//				if (j == testMap[0].length){
+//					g.fillRect(
+//							startX1 + ((j+1) * incrX / 2) + ((i+1) * incrX / 2), 
+//							startY1 + (i * incrY / 2) - (j * incrY / 2), 
+//							yCount, 
+//							yCount);
+//					yCount = yCount + 2;
+//				}
 				
 			}
 		}
 	}
 	
-	public void testDrawTrees(Graphics g){
-		g.drawImage(tree1, 200, 200, null);
+	//This method runs through the testObjects 2D Array checking if there is an object at each space. 
+	//When an object is found at a space in the Array, the drawObjectsFromArray() method is called to draw the object
+	//in the correct position.
+	public void getObjectsFromArray(Graphics g){
+		for (int i = 0; i < testMap.length; i++){
+			for (int j = testMap[0].length; j > 0; j--){
+				if (testObjects[i][j-1] == 1){
+					int startX = parentFrame.getWidth() / 4 - 30;
+					int startY = 45 + 200;
+					drawObjectsFromArray(g, tree1, startX, startY, i, j);
+				}
+				else if (testObjects[i][j-1] == 2){
+					int startX = parentFrame.getWidth() / 4;
+					int startY = 85 + 200;
+					drawObjectsFromArray(g, tree2, startX, startY, i, j);
+				}
+				else if (testObjects[i][j-1] == 3){
+					int startX = parentFrame.getWidth() / 4 + 40;
+					int startY = 85 + 300;
+					drawObjectsFromArray(g, bush1, startX, startY, i, j);
+				}
+				else if (testObjects[i][j-1] == 4){
+					int startX = parentFrame.getWidth() / 4 + 60;
+					int startY = 85 + 340;
+					drawObjectsFromArray(g, rock1, startX, startY, i, j);
+				}
+			}
+		}
+	}
+	
+	public void drawObjectsFromArray(Graphics g, Image image, int startX, int startY, int i, int j){
+		int incrX = 124;
+		int incrY = 70;
+		g.drawImage(image, 
+				startX + (j * incrX / 2) + (i * incrX / 2), 
+				startY + (i * incrY / 2) - (j * incrY / 2), 
+				null);
 	}
 	
 	public void redraw(Graphics g){
-		//drawGrass(g);
+		g.fillRect(0, 0, parentFrame.getWidth(), parentFrame.getHeight());
 		testDrawGrass(g);
-		testDrawTrees(g);
+		getObjectsFromArray(g);
 	}
 	
-	public viewDirection getViewDir(){
-		return viewDir;
-	}
-
-	public void setViewDir(viewDirection vd){
-		viewDir = vd;
-	}
 	
 	
 	
