58bfd2652c78927159af0b6a63b20d317025f7f7
dan.henton@live.com <dan.henton@Live.com>
2014-10-03 17:07:03 +1300

    Reading files is now fixed and reads them corretly. 	-csv file now requires a maxY, maxX for the game grid size 	-reader now takes the max sizes in creating the boardCell array

--- Cat and Mouse/src/catgame/logic/RoomBuilder.java
+++ Cat and Mouse/src/catgame/logic/RoomBuilder.java
@@ -8,53 +8,70 @@ import catgame.gameObjects.Rock;
 import catgame.gameObjects.Tree;
 import catgame.logic.GameUtil.Direction;
 
+/**
+ * 
+ * @author Dan Henton
+ * 
+ * Creates rooms by reading in .csv files adds objects and items for them game. 
+ * this is a helper class to get the rooms make for saving to xml
+ *
+ */
 public class RoomBuilder {
 	private String fileName;
 	private BoardCell[][] boardFile;
 
-	public RoomBuilder() {
-		boardFile = loadBoard();
+	public RoomBuilder(String fileName) {
+		boardFile = loadBoard(fileName);
 	}
 
-	public BoardCell[][] loadBoard() {
-		BoardCell[][] board = new BoardCell[6][6];
+	/**
+	 * Reads a .cvs file and creates a room for the game.
+	 * @param fileName
+	 * @return
+	 */
+	public BoardCell[][] loadBoard(String fileName) {
+		BoardCell[][] board = new BoardCell[0][0];
 		// make a new board from file
 		BufferedReader buffer;
 
 		int y = 0;
 		try {
-			buffer = new BufferedReader(new FileReader("SwenProjectRoomTestOne.csv"));
+			buffer = new BufferedReader(new FileReader(fileName));
+			//Set the formatting for the array
+			String formatingLine = buffer.readLine();
+			String[] formatArray = formatingLine.split(",");
+			int maxY = Integer.parseInt(formatArray[0]);
+			int maxX= Integer.parseInt(formatArray[1]);			
+			board = new BoardCell[maxY][maxX];
+			
 			loop: while (true) {
 				String line = buffer.readLine();
 				if (line == null) {
 					break loop;
 				}
 				String[] values = line.split(",");
+				
+				//TODO Make it a switch
 				for (int x = 0; x < values.length; x++) {
 					if (values[x].equals("0")) {
-						board[x][y] = addEmptyCell(x, y);
+						board[y][x] = addEmptyCell(x, y);
 					} else if (values[x].equals("1")) {
-						board[x][y] = addGrass(x, y);
+						board[y][x] = addGrass(x, y);
 					} else if (values[x].equals("2")) {
-						board[x][y] = addBush(x, y);
+						board[y][x] = addBush(x, y);
 					} else if (values[x].equals("3")) {
-						board[x][y] = addTree(x, y);
+						board[y][x] = addTree(x, y);
 					} else if (values[x].equals("4")) {
-						board[x][y] = addRock(x, y);
+						board[y][x] = addRock(x, y);
 					} else {
 						System.out.println(values[x]);
 					}
 				}
 				y++;
-				if(y == board.length){
-					break loop;
-				}
 			}
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-
-		String boardTest = "";
 
 		return board;
 	}
@@ -84,9 +101,9 @@ public class RoomBuilder {
 	}
 
 	public static void main(String[] args) {
-		// RoomBuilder buildBoard = new RoomBuilder();
-		// Room testRoom = new Room(1, buildBoard.getBoardFile());
-		// testRoom.printBoard();
+		 RoomBuilder buildBoard = new RoomBuilder("SwenProjectRoomTestOne.csv");
+		 Room testRoom = new Room(1, buildBoard.getBoardFile());
+		 testRoom.printBoard();
 		
 		
 		
