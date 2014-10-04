ddc45ea602eae7b38add62a167106b75d9e44bbb
MichaelGordon <fallthroughinfinity@gmail.com>
2014-10-05 00:38:03 +1300

    Images now render correctly when viewing direction is changed. Class is very messy and requires a lot of modifications to remove duplicate cod but at least it works correctly as of now.

--- Cat and Mouse/src/catgame/gui/renderpanel/RenderPanel.java
+++ Cat and Mouse/src/catgame/gui/renderpanel/RenderPanel.java
@@ -33,11 +33,11 @@ public class RenderPanel extends JPanel {
 	private Image catBackLeft1;
 	private Image catBackRight1;
 	
-	public static Direction viewDirection = Direction.NORTH;
+	public static Direction viewDirection = Direction.EAST;
 	
 	//Classes for creating a test board
-	RoomBuilder buildBoard;
-	Room testRoom;
+	private RoomBuilder buildBoard;
+	private Room testRoom;
 	
 	//Test 2D array for grassBlocks
 	private int[][] testMap = {
@@ -118,64 +118,229 @@ public class RenderPanel extends JPanel {
 	
 
 	
-	//testing method for drawing grass blocks form 2D array
-	public void testDrawGrass(Graphics g){
-		int startX1 = 19 + parentFrame.getWidth() / 4;
-		int startY1 = 20 + 400;
+	/**
+	 * Draws ground blocks based on the current viewDirection
+	 * @param g
+	 */
+	public void drawGround(Graphics g){
+		int startX = 19 + parentFrame.getWidth() / 4;
+		int startY = 20 + 400;
 		int incrX = 124;
 		int incrY = 70;
-		for (int i = 0; i < testRoom.getBoardGrid().length; i++){
-			for (int j = testRoom.getBoardGrid()[0].length; j > 0; j--){
-				if (testRoom.getBoardGrid()[i][j-1].getGroundType() == "Grass"){
-					g.drawImage(grassBlock, 
-							startX1 + (j * incrX / 2) + (i * incrX / 2), 
-							startY1 + (i * incrY / 2) - (j * incrY / 2), 
-							null);
+		int sendX;
+		int sendY;
+		for (int y = 0; y < testRoom.getBoardGrid().length; y++){
+			for (int x = testRoom.getBoardGrid()[0].length-1; x >= 0; x--){
+				if (viewDirection == Direction.NORTH){
+					sendX = testRoom.getBoardGrid()[0].length-x-1;
+					sendY = y;
+					if (testRoom.getBoardGrid()[sendY][sendX].getGroundType() == "Grass"){
+						g.drawImage(grassBlock, 
+								startX + (x * incrX / 2) + (y * incrX / 2), 
+								startY + (y * incrY / 2) - (x * incrY / 2), 
+								null);
+					}
+					if (testRoom.getBoardGrid()[sendY][sendX].getObjectOnCell() instanceof Tree){
+						int treeStartX = parentFrame.getWidth() / 4 - 30;
+						int treeStartY = 45 + 200;
+						g.drawImage(tree1, 
+								treeStartX + (x * incrX / 2) + (y * incrX / 2), 
+								treeStartY + (y * incrY / 2) - (x * incrY / 2), 
+								null);
+					}					
+				}
+				else if (viewDirection == Direction.WEST){
+					sendX = x;
+					sendY = y;
+					if (testRoom.getBoardGrid()[sendY][sendX].getGroundType() == "Grass"){
+						g.drawImage(grassBlock, 
+								startX + (x * incrX / 2) + (y * incrX / 2), 
+								startY + (y * incrY / 2) - (x * incrY / 2), 
+								null);
+					}
+					if (testRoom.getBoardGrid()[sendY][sendX].getObjectOnCell() instanceof Tree){
+						int treeStartX = parentFrame.getWidth() / 4 - 30;
+						int treeStartY = 45 + 200;
+						g.drawImage(tree1, 
+								treeStartX + (x * incrX / 2) + (y * incrX / 2), 
+								treeStartY + (y * incrY / 2) - (x * incrY / 2), 
+								null);
+					}
+				}
+				else if (viewDirection == Direction.SOUTH){
+					sendY = testRoom.getBoardGrid().length-y-1;
+					sendX = x;
+					if (testRoom.getBoardGrid()[sendY][sendX].getGroundType() == "Grass"){
+						g.drawImage(grassBlock, 
+								startX + (x * incrX / 2) + (y * incrX / 2), 
+								startY + (y * incrY / 2) - (x * incrY / 2), 
+								null);
+					}
+					if (testRoom.getBoardGrid()[sendY][sendX].getObjectOnCell() instanceof Tree){
+						int treeStartX = parentFrame.getWidth() / 4 - 30;
+						int treeStartY = 45 + 200;
+						g.drawImage(tree1, 
+								treeStartX + (x * incrX / 2) + (y * incrX / 2), 
+								treeStartY + (y * incrY / 2) - (x * incrY / 2), 
+								null);
+					}
+				}
+				else if (viewDirection == Direction.EAST){
+					sendY = testRoom.getBoardGrid().length-y-1;
+					sendX = testRoom.getBoardGrid()[0].length-x-1;
+					if (testRoom.getBoardGrid()[sendX][sendY].getGroundType() == "Grass"){
+						g.drawImage(grassBlock, 
+								startX + (x * incrX / 2) + (y * incrX / 2), 
+								startY + (y * incrY / 2) - (x * incrY / 2), 
+								null);
+					}
+					if (testRoom.getBoardGrid()[sendY][sendX].getObjectOnCell() instanceof Tree){
+						int treeStartX = parentFrame.getWidth() / 4 - 30;
+						int treeStartY = 45 + 200;
+						g.drawImage(tree1, 
+								treeStartX + (x * incrX / 2) + (y * incrX / 2), 
+								treeStartY + (y * incrY / 2) - (x * incrY / 2), 
+								null);
+					}
 				}
 			}
 		}
 	}
 	
-	public void getObjectsFromArray(Graphics g){
-		for (int i = 0; i < testRoom.getBoardGrid().length; i++){
-			for (int j = testRoom.getBoardGrid()[0].length; j > 0; j--){
-				if (testRoom.getBoardGrid()[i][j-1].getObjectOnCell() instanceof Tree){
-					int startX = parentFrame.getWidth() / 4 - 30;
-					int startY = 45 + 200;
-					drawObjectsFromArray(g, tree1, startX, startY, i, j);
-				}
-				else if (testRoom.getBoardGrid()[i][j-1].getObjectOnCell() instanceof Tree){
-					int startX = parentFrame.getWidth() / 4;
-					int startY = 85 + 200;
-					drawObjectsFromArray(g, tree2, startX, startY, i, j);
-				}
-				else if (testRoom.getBoardGrid()[i][j-1].getObjectOnCell() instanceof Bush){
-					int startX = parentFrame.getWidth() / 4 + 40;
-					int startY = 85 + 300;
-					drawObjectsFromArray(g, bush1, startX, startY, i, j);
-				}
-				else if (testRoom.getBoardGrid()[i][j-1].getObjectOnCell() instanceof Rock){
-					int startX = parentFrame.getWidth() / 4 + 60;
-					int startY = 85 + 340;
-					drawObjectsFromArray(g, rock1, startX, startY, i, j);
-				}
-				else if (testRoom.getBoardGrid()[i][j-1].getObjectOnCell() instanceof PlayableCharacter){
-					int startX = parentFrame.getWidth() / 4 + 60;
-					int startY = 85 + 340;
-					drawObjectsFromArray(g, catFrontLeft1, startX, startY, i, j);
-				}
-			}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	public void drawObject(Graphics g, int sendY, int sendX){
+		if (testRoom.getBoardGrid()[sendY][sendX].getObjectOnCell() instanceof Tree){
+			int startX = parentFrame.getWidth() / 4 - 30;
+			int startY = 45 + 200;
+			drawObjectsFromArray(g, tree1, startX, startY, sendY, sendX);
+		}
+		else if (testRoom.getBoardGrid()[sendY][sendX].getObjectOnCell() instanceof Tree){
+			int startX = parentFrame.getWidth() / 4;
+			int startY = 85 + 200;
+			drawObjectsFromArray(g, tree2, startX, startY, sendY, sendX);
+		}
+		else if (testRoom.getBoardGrid()[sendY][sendX].getObjectOnCell() instanceof Bush){
+			int startX = parentFrame.getWidth() / 4 + 40;
+			int startY = 85 + 300;
+			drawObjectsFromArray(g, bush1, startX, startY, sendY, sendX);
+		}
+		else if (testRoom.getBoardGrid()[sendY][sendX].getObjectOnCell() instanceof Rock){
+			int startX = parentFrame.getWidth() / 4 + 60;
+			int startY = 85 + 340;
+			drawObjectsFromArray(g, rock1, startX, startY, sendY, sendX);
 		}
 	}
 	
-	public void drawObjectsFromArray(Graphics g, Image image, int startX, int startY, int i, int j){
+	public void drawGroundFromArray(Graphics g, Image image, int startX, int startY, int y, int x){
+		int incrX = 124;
+		int incrY = 70;
+		g.drawImage(grassBlock, 
+				startX + (x * incrX / 2) + (y * incrX / 2), 
+				startY + (y * incrY / 2) - (x * incrY / 2), 
+				null);
+	}
+	
+	public void drawObjectsFromArray(Graphics g, Image image, int startX, int startY, int sendY, int sendX){
 		int incrX = 124;
 		int incrY = 70;
 		g.drawImage(image, 
-				startX + (j * incrX / 2) + (i * incrX / 2), 
-				startY + (i * incrY / 2) - (j * incrY / 2), 
+				startX + (sendX * incrX / 2) + (sendY * incrX / 2), 
+				startY + (sendY * incrY / 2) - (sendX * incrY / 2), 
 				null);
 	}
+	
+	public void drawCharacters(Graphics g){
+		g.drawImage(catFrontLeft1, 710, 325, null);
+	}
+
+	
+	public void redraw(Graphics g){
+		g.fillRect(0, 0, parentFrame.getWidth(), parentFrame.getHeight());
+		drawGround(g);
+		//getObjectsFromArray(g);
+		drawCharacters(g);
+	}
+	
+	
+//	public void getObjectsFromArray(Graphics g){
+//	for (int y = 0; y < testRoom.getBoardGrid().length; y++){
+//		for (int x = testRoom.getBoardGrid()[0].length; x > 0; x--){
+//			if (viewDirection == Direction.NORTH){
+//				if (testRoom.getBoardGrid()[y][testRoom.getBoardGrid()[0].length-x].getGroundType() == "Grass"){
+//					g.drawImage(grassBlock, 
+//							startX1 + (x * incrX / 2) + (y * incrX / 2), 
+//							startY1 + (y * incrY / 2) - (x * incrY / 2), 
+//							null);
+//				}
+//			}
+//			else if (viewDirection == Direction.WEST){
+//				if (testRoom.getBoardGrid()[y][x-1].getGroundType() == "Grass"){
+//					g.drawImage(grassBlock, 
+//							startX1 + (x * incrX / 2) + (y * incrX / 2), 
+//							startY1 + (y * incrY / 2) - (x * incrY / 2), 
+//							null);
+//				}
+//			}
+//			else if (viewDirection == Direction.SOUTH){
+//				if (testRoom.getBoardGrid()[testRoom.getBoardGrid().length-y-1][x-1].getGroundType() == "Grass"){
+//					g.drawImage(grassBlock, 
+//							startX1 + (x * incrX / 2) + (y * incrX / 2), 
+//							startY1 + (y * incrY / 2) - (x * incrY / 2), 
+//							null);
+//				}
+//			}
+//			else if (viewDirection == Direction.EAST){
+//				if (testRoom.getBoardGrid()[testRoom.getBoardGrid().length-y-1][testRoom.getBoardGrid()[0].length-x].getGroundType() == "Grass"){
+//					g.drawImage(grassBlock, 
+//							startX1 + (x * incrX / 2) + (y * incrX / 2), 
+//							startY1 + (y * incrY / 2) - (x * incrY / 2), 
+//							null);
+//				}
+//			}
+//		}
+//	}
+//	
+//	for (int i = 0; i < testRoom.getBoardGrid().length; i++){
+//		for (int j = testRoom.getBoardGrid()[0].length; j > 0; j--){
+//			if (testRoom.getBoardGrid()[i][j-1].getObjectOnCell() instanceof Tree){
+//				int startX = parentFrame.getWidth() / 4 - 30;
+//				int startY = 45 + 200;
+//				drawObjectsFromArray(g, tree1, startX, startY, i, j);
+//			}
+//			else if (testRoom.getBoardGrid()[i][j-1].getObjectOnCell() instanceof Tree){
+//				int startX = parentFrame.getWidth() / 4;
+//				int startY = 85 + 200;
+//				drawObjectsFromArray(g, tree2, startX, startY, i, j);
+//			}
+//			else if (testRoom.getBoardGrid()[i][j-1].getObjectOnCell() instanceof Bush){
+//				int startX = parentFrame.getWidth() / 4 + 40;
+//				int startY = 85 + 300;
+//				drawObjectsFromArray(g, bush1, startX, startY, i, j);
+//			}
+//			else if (testRoom.getBoardGrid()[i][j-1].getObjectOnCell() instanceof Rock){
+//				int startX = parentFrame.getWidth() / 4 + 60;
+//				int startY = 85 + 340;
+//				drawObjectsFromArray(g, rock1, startX, startY, i, j);
+//			}
+//		}
+//	}
+//}
+	
+	
+	
+	
+	
+	
 	
 	
 //	//testing method for drawing grass blocks form 2D array
@@ -234,16 +399,6 @@ public class RenderPanel extends JPanel {
 //				startY + (i * incrY / 2) - (j * incrY / 2), 
 //				null);
 //	}
-	
-	public void redraw(Graphics g){
-		g.fillRect(0, 0, parentFrame.getWidth(), parentFrame.getHeight());
-		testDrawGrass(g);
-		getObjectsFromArray(g);
-	}
-	
-	
-	
-	
 	
 
 
