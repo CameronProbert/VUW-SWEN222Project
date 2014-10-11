54dfbd3c7ee0c471a707c0ef9507a7256dead775
CameronProbert <c.probert@hotmail.co.nz>
2014-10-11 14:30:41 +1300

    Added OpenFile method to helper methods

--- Cat and Mouse/src/catgame/gui/HelperMethods.java
+++ Cat and Mouse/src/catgame/gui/HelperMethods.java
@@ -1,15 +1,16 @@
 package catgame.gui;
 
+import java.awt.Component;
 import java.awt.Dimension;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class HelperMethods {
-
-
 
 	// ================= Helper Methods ====================
 
@@ -74,33 +75,50 @@ public class HelperMethods {
 
 		return string;
 	}
-	
+
 	/**
 	 * Creates a slider that returns a number
+	 * 
 	 * @return
 	 */
-	public static int getNumSliderDialog(String title, int minNum, int maxNum, int defaultNum){
+	public static int getNumSliderDialog(String title, int minNum, int maxNum,
+			int defaultNum) {
 		// Get total number of players
 		JPanel playerPanel = new JPanel();
 		playerPanel.setPreferredSize(new Dimension(200, 200));
-		JSlider slider = new JSlider(JSlider.HORIZONTAL, minNum, maxNum, defaultNum);
+		JSlider slider = new JSlider(JSlider.HORIZONTAL, minNum, maxNum,
+				defaultNum);
 		slider.setMajorTickSpacing(1);
 		slider.setPaintTicks(true);
 		slider.setPaintLabels(true);
 
 		// Dialog asking how many players there are
 		if (JOptionPane.CLOSED_OPTION == JOptionPane.showConfirmDialog(null,
-				slider, title,
-				JOptionPane.PLAIN_MESSAGE)) {
+				slider, title, JOptionPane.PLAIN_MESSAGE)) {
 			System.exit(0);
 		}
 		return slider.getValue();
 	}
 
 	public static void textDialog(String title, String message) {
-		JOptionPane.showMessageDialog(null,
-				message, title,
+		JOptionPane.showMessageDialog(null, message, title,
 				JOptionPane.PLAIN_MESSAGE);
 	}
 
+	public static String openFile(String description, Component parent,
+			String... extension) {
+		JFileChooser chooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(
+				description, extension);
+		chooser.setFileFilter(filter);
+		int returnVal = -1;
+		do {
+			returnVal = chooser.showOpenDialog(parent);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				System.out.println("You chose to open this file: "
+						+ chooser.getSelectedFile().getAbsolutePath());
+			}
+		} while (returnVal != JFileChooser.APPROVE_OPTION);
+		return chooser.getSelectedFile().getAbsolutePath();
+	}
 }
