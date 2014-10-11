e4df6c15cd5f7b91b942f51a801dd6b85df320b5
Milarr <mila.dymnikova@gmail.com>
2014-10-11 15:46:23 +1300

    Working on making a helper class/method to help me parse a string that contains info on the boardCell and then make it into BoardCell[][]

---
+++ Cat and Mouse/src/catgame/dataStorage/LoadingHelper.java
@@ -0,0 +1,24 @@
+package catgame.dataStorage;
+
+import catgame.gameObjects.*;
+import catgame.logic.*;
+
+import org.jdom2.*;
+import org.jdom2.input.SAXBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class LoadingHelper {
+
+	public LoadingHelper() {
+
+	}
+
+	public BoardCell loadBoardCell(String boardString) {
+		// get (x, y, ID, groundType)
+		//String[] boardInfo =  
+		return null;
+	}
+}
